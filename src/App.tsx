21a56cca29487cb38414ceb729170608def4f776
black <jaberhc2002@gmail.com>
2023-12-27 00:29:28 +0600

    add : added a svg animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 const App = () => {
   return (
-    <div>
-      <div>App</div>
+    <div className="w-full min-h-screen flex justify-center items-center flex-col relative">
+      <svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
+        <circle
+          id="svg"
+          cx="400"
+          cy="400"
+          fill="none"
+          r="293"
+          stroke-width="38"
+          stroke="#E387FF"
+          stroke-dasharray="1841 1400"
+          stroke-linecap="round"
+        />
+      </svg>
     </div>
   );
 };
