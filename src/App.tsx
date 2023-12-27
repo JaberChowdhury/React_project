f6267af999430714dd06bd360a508abacef3c0f0
black <jaberhc2002@gmail.com>
2023-12-27 20:28:50 +0600

    add : added some svg file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 const App = () => {
   return (
     <div className="w-full min-h-screen flex justify-center items-center flex-col relative">
-      <svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
-        <circle
-          id="svg"
-          cx="400"
-          cy="400"
-          fill="none"
-          r="293"
-          stroke-width="38"
-          stroke="#E387FF"
-          stroke-dasharray="1841 1400"
-          stroke-linecap="round"
-        />
-      </svg>
+      <img src="./loader.svg" alt="loader.svg" />
+      <img src="./jaber.svg" alt="jaber.svg" />
     </div>
   );
 };
