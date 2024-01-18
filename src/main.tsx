357e22efa91d29597ff3e5c45c7131c196553d15
black <jaberhc2002@gmail.com>
2024-01-19 02:20:29 +0600

    fix : add lazy ...

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import "./index.css";
 
 import { createRoot } from "react-dom/client";
-// import { Routes } from '@generouted/react-router'
-import { Routes } from "@generouted/react-router/lazy"; // route-based code-splitting
+import { Routes } from "@generouted/react-router";
+// import { Routes } from "@generouted/react-router/lazy"; // route-based code-splitting
 
 createRoot(document.getElementById("root")!).render(<Routes />);
