e2eec9b6b7feab01924f5fd030fc99d2fc2275c8
black <jaberhc2002@gmail.com>
2024-01-30 23:01:35 +0600

    add : page loading indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "@/components/Navbar";
+import ProgressBar from "@badrap/bar-of-progress";
+import { useNavigation } from "react-router-dom";
 
 export default function App() {
+  const progress = new ProgressBar({
+    size: 2,
+    color: "#29e",
+    delay: 1,
+  });
+
+  const { state } = useNavigation();
+  console.log(useNavigation());
+
+  if (state === "idle") {
+    progress.start();
+  }
+
+  if (state !== "loading") {
+    setTimeout(() => {
+      progress.finish();
+    }, 500);
+  }
+
   return (
     <section>
       <Navbar />
