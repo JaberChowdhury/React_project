0374879afee7d1eddd9eff6256c9da499818fe29
black <jaberhc2002@gmail.com>
2024-02-14 22:00:16 +0600

    add : configure with swc

--- fresh/vite.config.ts
+++ fresh/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from "path";
+// import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import generouted from "@generouted/react-router/plugin";
 
 export default defineConfig({
   plugins: [react(), generouted()],
+   resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
--- with-shadcn/vite.config.ts
+++ with-shadcn/vite.config.ts
@@ -1,5 +1,6 @@
 import path from "path";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import generouted from "@generouted/react-router/plugin";
 
