87486abe81e4655125d12bf1e9744adc81e68961
black <jaberhc2002@gmail.com>
2024-05-30 15:07:15 +0000

    format : with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const App = () => {
-  return(
+  return (
     <div>
       <div>App</div>
     </div>
-    )
-}
+  );
+};
 
 export default App;
