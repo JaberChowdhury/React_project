63900aeacf89d32d9a358ac4bb2ef6ab9c74ecc0
black <jaberhc2002@gmail.com>
2024-06-03 15:52:52 +0000

    add : basic mui comp9nents

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import { Typography } from "@mui/material";
+
 const App = () => {
   return (
     <div>
-      <div>App</div>
+      <Typography variant="h1">App</Typography>
     </div>
   );
 };
