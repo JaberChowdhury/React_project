0dc24c00b9b8b57937150a5eca23edcfb8e43472
black <jaberhc2002@gmail.com>
2024-10-30 20:23:37 +0600

    add : example

--- with-fluentui/src/pages/index.tsx
+++ with-fluentui/src/pages/index.tsx
@@ -1,9 +1,13 @@
-const Home = () => {
+import { Button, Spinner, PrimaryButton } from "@fluentui/react";
+
+const App = () => {
   return (
     <div>
-      <div>Awesome</div>
+      <Button>Follow</Button>
+      <PrimaryButton>Follow</PrimaryButton>
+      <Spinner></Spinner>
     </div>
   );
 };
 
-export default Home;
+export default App;
