26777a7bacf6232741dd04578ebf9cbd6933a08f
black <jaberhc2002@gmail.com>
2024-10-30 20:59:16 +0600

    new project

--- fresh/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div>
-      <div>Awesome</div>
-    </div>
-  );
-};
-
-export default Home;
--- with-daisyui/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div>
-      <button className="btn">Awesome daisyui</button>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
-}
