305901172af944b6f8f50603db3cb3ac1c8a35e2
Black Light <78747748+JaberChowdhury@users.noreply.github.com>
2025-01-19 12:17:23 +0600

    refactor : remove ESLint configuration and update project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button, Card, Container, Link, Typography } from "@mui/material";
 
-import { Typography } from "@mui/material";
+function App() {
+  const [count, setCount] = useState(0);
 
-const App = () => {
   return (
-    <div>
-      <Typography variant="h1">App</Typography>
-    </div>
+    <>
+      <Container>
+        <Link href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </Link>
+        <Link href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </Link>
+      </Container>
+      <Typography variant="h1">Vite + React</Typography>
+      <Card className="card">
+        <Button
+          variant="contained"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </Button>
+        <Typography variant="h6">
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </Typography>
+      </Card>
+      <Typography variant="h6" className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </Typography>
+    </>
   );
-};
+}
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { StrictMode } from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Container } from "@mui/material";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Container>
-      <App />
-    </Container>
-  </React.StrictMode>,
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
