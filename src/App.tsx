6f86d17cf52d01019971d8396154a9c728b50190
Black Light <78747748+JaberChowdhury@users.noreply.github.com>
2025-01-29 20:02:07 +0600

    refactor: remove unused CSS files and update package dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,192 @@
-import { useState, useCallback, useMemo } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button, Card, Container, Link, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  IconButton,
+  Slider,
+  Stack,
+  Switch,
+  Typography,
+} from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import useStore from "./store/store";
+import { useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const {
+    password,
+    isNumber,
+    isEmoji,
+    isLowerCase,
+    isSpecialCharacter,
+    password_length,
+    isUpperCase,
+    toggleNumber,
+    toggleLowerCase,
+    toggleEmoji,
+    increasePasswordLength,
+    decreasePasswordLength,
+    toggleSpecialCharac,
+    toggleUpperCase,
+    updatePasswordLength,
+    updatePassword,
+  } = useStore();
 
-  const handleClick = useCallback(() => {
-    setCount((count) => count + 1);
-  }, [setCount]);
-
-  const memoizedContainer = useMemo(
-    () => (
-      <Container>
-        <Link href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" loading="lazy" />
-        </Link>
-        <Link href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-            loading="lazy"
-          />
-        </Link>
-      </Container>
-    ),
-    []
-  );
-
-  const memoizedButton = useMemo(
-    () => (
-      <Button variant="contained" onClick={handleClick}>
-        count is {count}
-      </Button>
-    ),
-    [handleClick, count]
-  );
-
-  const memoizedTypography1 = useMemo(
-    () => <Typography variant="h1">Vite + React</Typography>,
-    []
-  );
-
-  const memoizedTypography2 = useMemo(
-    () => (
-      <Typography variant="h6" className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </Typography>
-    ),
-    []
-  );
-
-  const memoizedCard = useMemo(
-    () => (
-      <Card className="card">
-        {memoizedButton}
-        <Typography variant="h6">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </Typography>
-      </Card>
-    ),
-    [memoizedButton]
-  );
-
+  useEffect(() => {
+    updatePassword();
+  }, [
+    isNumber,
+    isEmoji,
+    isLowerCase,
+    isSpecialCharacter,
+    password_length,
+    isUpperCase,
+  ]);
   return (
-    <>
-      {memoizedContainer}
-      {memoizedTypography1}
-      {memoizedCard}
-      {memoizedTypography2}
-    </>
+    <Stack
+      sx={{
+        width: "100%",
+        minHeight: "100vh",
+        background:
+          "-webkit-linear-gradient(135deg, hsla(173, 89%, 70%, 1) 0%, hsla(196, 68%, 38%, 1) 100%)",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Card
+        sx={{
+          width: { sm: "100%", md: "80%", xl: "60%" },
+          backgroundColor: "rgba(255,255,255,0.5)",
+          backdropFilter: "blur(10px)",
+        }}
+      >
+        <CardContent
+          component={Stack}
+          sx={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <Stack
+            direction="row"
+            sx={{ justifyContent: "center", alignItems: "center" }}
+          >
+            <Typography
+              sx={{
+                border: "1px solid black",
+                minWidth: { sm: "100%", lg: "700px" },
+                textAlign: "center",
+                p: 1,
+              }}
+            >
+              {password}
+            </Typography>
+            <IconButton color="primary" aria-label="add to shopping cart">
+              <ContentCopyIcon />
+            </IconButton>
+          </Stack>
+          <CardContent sx={{ width: "70%" }}>
+            <Stack
+              direction="row"
+              sx={{ justifyContent: "center", alignItems: "center" }}
+            >
+              <Slider
+                min={4}
+                max={200}
+                value={password_length}
+                onChange={(_, v) => updatePasswordLength(v)}
+              />
+              <Stack direction="row">
+                <IconButton onClick={decreasePasswordLength}>
+                  <RemoveIcon />
+                </IconButton>
+                <input
+                  style={{
+                    textAlign: "center",
+                  }}
+                  type="number"
+                  min="4"
+                  max="200"
+                  value={password_length}
+                  onChange={(e) =>
+                    updatePasswordLength(
+                      Math.min(Math.max(Number(e.target.value), 4), 200)
+                    )
+                  }
+                />
+                <IconButton onClick={increasePasswordLength}>
+                  <AddIcon />
+                </IconButton>
+              </Stack>
+            </Stack>
+          </CardContent>
+          <CardContent>
+            <Box
+              sx={{
+                border: "1px solid",
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fit,minmax(300px,1fr))",
+                justifyContent: "center",
+              }}
+            >
+              <CardContent
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Typography>Number</Typography>
+                <Switch onChange={toggleNumber} checked={isNumber} />
+              </CardContent>
+              <CardContent
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Typography>Emoji</Typography>
+                <Switch onChange={toggleEmoji} checked={isEmoji} />
+              </CardContent>
+              <CardContent
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Typography>UpperCase</Typography>
+                <Switch onChange={toggleUpperCase} checked={isUpperCase} />
+              </CardContent>
+              <CardContent
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Typography>LowerCase</Typography>
+                <Switch onChange={toggleLowerCase} checked={isLowerCase} />
+              </CardContent>
+              <CardContent
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Typography>Special Character</Typography>
+                <Switch
+                  onChange={toggleSpecialCharac}
+                  checked={isSpecialCharacter}
+                />
+              </CardContent>
+            </Box>
+          </CardContent>
+        </CardContent>
+      </Card>
+    </Stack>
   );
-}
+};
 
 export default App;
