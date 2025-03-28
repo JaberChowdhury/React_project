4c8fa6e044fa0c59b5f8f23c954edc4899f7aa4a
Black Light <78747748+JaberChowdhury@users.noreply.github.com>
2025-03-28 23:28:40 +0600

    feat: add utility functions and components for UI elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import { Card } from './components/ui/card';
+import { Button } from './components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      {' '}
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
@@ -15,14 +16,14 @@ function App() {
         </a>
       </div>
       <h1>Rspack + React + TypeScript</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
+      <Card className="card">
+        <Button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </Card>
       <p className="read-the-docs">
         Click on the Rspack and React logos to learn more
       </p>
